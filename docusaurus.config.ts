f162fa92e7f604bfffbe6b239e8c9cc668679974
Administrator <jczap.dev@gmail.com>
2026-01-18 23:07:27 -0500

    chore: update Docusaurus configuration and remove unused CSS component
    Changed the site URL and organization name in docusaurus.config.ts. Removed the index.tsx file which contained the HomepageHeader component, streamlining the project structure.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,14 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://nexusevo.com',
+  url: 'https://nexus.zjceo.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'nexusevo', // Usually your GitHub org/user name.
+  organizationName: 'zjceo', // Usually your GitHub org/user name.
   projectName: 'nexusevo', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -46,7 +46,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/zjceo/nexusevo/tree/main/docs',
         },
         theme: {
           customCss: './src/css/custom.css',
